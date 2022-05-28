51bde3395343712c03657e9cbb92622ff68753b9
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-05-28 07:59:54 +0200

    git ignore

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require("dotenv").config()
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
@@ -12,7 +13,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://get-gears:xMPrZ2brvnbVymbJ@cluster0.cbcnz.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cbcnz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
